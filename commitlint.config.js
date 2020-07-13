e955d6216ea8aa06723e300ca10c1072b86d96d2
xiaowei <xiaowei@dtstack.com>
2020-07-13 16:18:34 +0800

    build: update rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     extends: ['@commitlint/config-angular'],
     'rules': {
+        'type-empty': [0, 'always'],
+        'subject-empty': [0, 'always'],
         'type-case': [2, 'always', 'lower-case'],
         'scope-case': [0, 'always']
     }
